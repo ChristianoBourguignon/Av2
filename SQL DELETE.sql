e241110c937dfd5f52e083ef973ae35bb5f4cb00
ChristianoBourguignon <christianombl@gmail.com>
2025-06-23 13:00:44 -0300

    feat: sql update and sql delete

---
+++ SQL DELETE.sql
@@ -0,0 +1,5 @@
+-- Deletar uma reserva
+DELETE FROM Reserva WHERE id_reserva = 1;
+
+-- Deletar um carro
+DELETE FROM Carro WHERE id_carro = 1;
---
+++ SQL UPDATE.sql
@@ -0,0 +1,5 @@
+-- Atualizar status do carro
+UPDATE Carro SET status = 'alugado' WHERE id_carro = 1;
+
+-- Atualizar dados do cliente
+UPDATE Cliente SET telefone = '21977777777' WHERE id_cliente = 1;
